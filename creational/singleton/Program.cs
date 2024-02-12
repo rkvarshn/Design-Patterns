dafe1afa0e40e30caa0f6628e8a89bb92a9c4aa9
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-02-12 09:13:36 +0530

    singleton pattern

---
+++ creational/singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿class MyClass
+{
+    private static readonly object _locker = new();
+    private static MyClass? _instance = null;
+    private MyClass()
+    {
+        Console.WriteLine("Constructor called");
+    }
+    public static MyClass GetInstance()
+    {
+        if(_instance == null)
+        {
+            lock(_locker) // lock is used to prevent other threads to create instance at the same time
+            {
+                if(_instance == null) // this prevents other instances waiting on lock to re-initialize, adding an extra layer of safety
+                {
+                    _instance = new MyClass();
+                }
+            }
+        }
+        return _instance;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass.GetInstance(); // only first time constructor is called
+        MyClass.GetInstance(); // constructor will nor be called as instance created by previous call will be returned
+    }
+}
